a8c5fa73e84f9729fb23ab8535839d3adf57dad4
yifan lin <linyifan1204@gmail.com>
2023-03-13 05:09:17 -0500

    delete not working terraform file

--- main.tf
+++ main.tf
@@ -1,36 +1,18 @@
-# Define the Docker Compose file
-resource "docker_compose_project" "app" {
-  name  = "nodemongo"
-  file  = "${path.module}/docker-compose.yml"
-}
+# terraform init
 
-# Start the containers
-resource "docker_compose_service" "app" {
-  name   = "app"
-  project_name = "${docker_compose_project.app.name}"
-  depends_on = ["mongo"]
+# 1. docker provider 
+# 2. resource docker image for mongodb
+# 3. resource docker container for mongodb
 
-  # Map the ports
-  ports {
-    internal = 3000
-    external = 3000
-  }
-}
+# terraform apply
+# terraform destroy
 
-resource "docker_compose_service" "mongo" {
-  name   = "mongo"
-  project_name = "${docker_compose_project.app.name}"
 
-  # Map the ports
-  ports {
-    internal = 27017
-    external = 27017
-  }
+# or:
 
-  # Mount the data volume
-  volumes {
-    container_path = "/data/db"
-    host_path      = "${path.module}/data"
-    read_only      = false
-  }
-}
+# provisioner:
+# command ="docker start mongo"
+# when ....
+
+# command = "docker stop mongo"
+# when ....
